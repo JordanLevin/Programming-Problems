caffd4b5ee72757382c262e250487e4d7c65d7d2
JordanLevin <jlevin38@binghamton.edu>
2017-05-14 15:46:02 -0400

    worked on more problems in 31-41 section

--- Haskell-Practice-Problems/practice31-41.hs
+++ Haskell-Practice-Problems/practice31-41.hs
@@ -1,7 +1,7 @@
 
 -- Problem 31
 isPrime :: Int -> Bool
-isPrime 2 = False
+isPrime 2 = True
 isPrime x
     | length (filter (== 0) (map (\z -> x `mod` z) nums)) > 0 = False
     | otherwise = True
@@ -17,4 +17,27 @@ myGcd x y
         lo = min x y
 
 -- Problem 33
+coprime :: Int -> Int -> Bool
+coprime x y = if myGcd x y == 1 then True else False
 
+-- Problem 34
+totient :: Int -> Int
+totient x = length (filter (coprime x) [1..x])
+
+-- Problem 35
+primeFactors :: Int -> [Int]
+primeFactors x
+    | x == firstPrime x 2 = [x]
+    | otherwise = firstPrime x 2 : primeFactors (x `div` firstPrime x 2)
+
+firstPrime :: Int -> Int -> Int
+firstPrime x y 
+    | isPrime y && mod x y == 0 = y 
+    | otherwise = firstPrime x (y+1)
+
+-- Problem 36
+primeFactorsMult :: Int -> [[Int]]
+primeFactorsMult x
+    |
+    where
+        factors = primeFactors x
