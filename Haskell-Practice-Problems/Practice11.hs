7902cf61a1e45b42f416a086ae0e33513c2053c0
Jordan <jlevin38@binghamton.edu>
2017-06-29 23:01:50 +0000

    completed haskell problem 15, 16 and 17 still not working

--- Haskell-Practice-Problems/Practice11.hs
+++ Haskell-Practice-Problems/Practice11.hs
@@ -26,3 +26,33 @@ decodeModified (x:xs) = decodeHelper x ++ decodeModified xs
 dupli :: [a] -> [a]
 dupli [x] = [x, x]
 dupli (x:xs) = [x, x] ++ dupli xs
+
+-- Question 15
+repli :: [a] -> Int -> [a]
+repli [] n = []
+repli x 1 = x
+repli [x] n = [x] ++ repli [x] (n-1)
+repli (x:xs) n = (repli [x] n) ++ (repli xs n)
+
+-- Question 16
+-- Not working at the moment
+{-dropOther :: [a] -> Int -> [a]-}
+{-dropOther x 0 = []-}
+{-dropOther x 1 = []-}
+{-dropOther x n = dropHelper x n 0 []-}
+{--- Helper function takes an additional int representing position in the list-}
+{-dropHelper :: a -> Int -> Int -> a -> a-}
+{-dropHelper x n count result-}
+    {-| count == length x = result-}
+    {-| count `mod` n /= 0 = (dropHelper x n (count+1) (result ++ x)) !! count-}
+    {-| otherwise = dropHelper x n (count+1) result-}
+
+-- Question 17
+split :: [a] -> Int -> [a]
+split list slice position = splitHelper list slice position []
+
+splitHelper :: a -> Int -> Int -> a -> a
+splitHelper list slice position accumulate
+    | position < slice = (splitHelper list slice (position+1) accumulate) ++ (list !! position)
+    | position == slice = accumulate
+
